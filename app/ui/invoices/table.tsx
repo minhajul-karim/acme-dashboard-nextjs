3e8441215e3331888e4f522126474302a0a813ba
Minhajul Karim <project@zaagsys.com>
2024-11-26 08:40:20 +0600

    Write interface for Invoice inside Table component

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -4,6 +4,17 @@ import InvoiceStatus from '@/app/ui/invoices/status';
 import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
 
+interface Invoice {
+  id: string;
+  amount: number;
+  date: string;
+  status: 'paid' | 'pending';
+  name: string;
+  email: string;
+  image_url: string;
+}
+
+
 export default async function InvoicesTable({
   query,
   currentPage,
@@ -18,7 +29,9 @@ export default async function InvoicesTable({
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
-            {invoices?.map((invoice) => (
+            {invoices?.map((invoice: Invoice) => {
+              console.log(invoice)
+              return (
               <div
                 key={invoice.id}
                 className="mb-2 w-full rounded-md bg-white p-4"
@@ -52,7 +65,7 @@ export default async function InvoicesTable({
                   </div>
                 </div>
               </div>
-            ))}
+            )})}
           </div>
           <table className="hidden min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
@@ -78,7 +91,7 @@ export default async function InvoicesTable({
               </tr>
             </thead>
             <tbody className="bg-white">
-              {invoices?.map((invoice) => (
+              {invoices?.map((invoice: Invoice) => (
                 <tr
                   key={invoice.id}
                   className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
