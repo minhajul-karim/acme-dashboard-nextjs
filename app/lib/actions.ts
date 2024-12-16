5c7f8f1883c6b74c707fd1ad626563776b7a1669
Minhajul Karim <project@zaagsys.com>
2024-12-16 09:06:00 +0600

    Write a server action to create invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,18 +17,18 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+  const rawFormData = {
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
-  });
-
+  }
+  const { customerId, amount, status, } = CreateInvoice.parse(rawFormData);
   const amountInCents = amount * 100;
-  const date = new Date().toISOString().split("T")[0];
+  const date = new Date().toISOString().split('T')[0];
 
   await connectionPool.query(`
     INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES ('${customerId}', '${amountInCents}', '${status}', '${date}')
+    VALUES ('${customerId}', '${amountInCents}', '${status}', '${date}')  
   `);
 
   revalidatePath("/dashboard/invoices");
