42e7e6ce4b467c4c95c1659349bff6123d582f18
Minhajul Karim <project@zaagsys.com>
2025-01-01 11:27:03 +0600

    AUTH Write server action to authenticate users using Next Auth

---
+++ auth.ts
@@ -0,0 +1,46 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
+import bcrypt from "bcrypt";
+import { User } from "./app/lib/definitions";
+
+const connectionPool = require("./db");
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await connectionPool.query(`
+      SELECT * from users WHERE email='${email}'
+    `);
+    return user.rows[0];
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to fetch user.");
+  }
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [Credentials({
+    async authorize(credentials) {
+      const parsedCredentials = z
+      .object({
+        email: z.string().email(),
+        password: z.string().min(6),
+      }).safeParse(credentials);
+      if (parsedCredentials.success) {
+        const { email, password } = parsedCredentials.data;
+        const user = await getUser(email);
+        if (!user) {
+          return null;
+        }
+        const passwordMatched = await bcrypt.compare(password, user.password);
+        if (passwordMatched) {
+          return user;
+        }
+      }
+      console.error("Invalid credentails");
+      return null;
+    }
+  })]
+});
