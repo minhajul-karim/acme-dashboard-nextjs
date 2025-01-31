e78d213ebafc85906e583759b1786de407f1c07e
Minhajul Karim <project@zaagsys.com>
2025-01-31 08:51:16 +0600

    Update the app description on the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -18,11 +18,12 @@ export default function Page() {
           <p
             className={`${montserrat.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
+            <strong>Welcome to Acme.</strong> An app for managing invoices,
+            tracking analytics, and handling customer data, inspired by the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              Next.js Learn Course example
             </a>
-            , brought to you by Vercel.
+            .
           </p>
           <Link
             href="/login"
