126e5616e6555bd4ce63eb917107752ddd9830e1
Minhajul Karim <project@zaagsys.com>
2025-02-04 16:20:37 +0600

    CUSTOMER Create initial setup for the customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import Search from "@/app/ui/search";
 import { Metadata } from "next";
+import { montserrat } from "@/app/ui/fonts";
+import { CreateCustomer } from "@/app/ui/invoices/buttons";
 
 export const metadata: Metadata = {
   title: "Customers"
 }
 
 export default function Customers() {
-  return <p>Customers Page</p>;
+  return (
+    <div>
+      <h1 className={`${montserrat.className} text-2xl`}>Customers</h1>
+      <div className="mt-4 md:mt-8 flex justify-between items-center gap-2">
+        <Search placeholder="Search customers..." />
+        <CreateCustomer />
+      </div>
+    </div>
+  );
 }
