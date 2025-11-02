f9f94de7a5a3c7b1388997d4d6e0a76c818987f3
Minhajul Karim <project@zaagsys.com>
2025-11-02 11:31:15 +0600

    feat: create primary setup for the signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,13 +1,13 @@
-import LoginForm from "../ui/login-form";
+import SignupForm from "../ui/signup-form";
 import AcmeLogo from "../ui/acme-logo";
 import { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "Log in",
+  title: "Sign up",
 };
 
-export default function Login() {
+export default function Signup() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
@@ -16,10 +16,10 @@ export default function Login() {
             <AcmeLogo />
           </div>
         </div>
-        <LoginForm />
+        <SignupForm />
         <p className="text-center font-bold">
-          Don't have an accout?
-          <Link className="text-blue-500 underline" href="/signup"> Sign up</Link>
+          Already have an accout?
+          <Link className="text-blue-500 underline" href="/login"> Log in</Link>
         </p>
       </div>
     </main>
---
+++ app/ui/signup-form.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { FormEvent, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { montserrat } from '@/app/ui/fonts';
+import {
+  AtSymbolIcon,
+  KeyIcon,
+} from '@heroicons/react/24/outline';
+import { ArrowRightIcon } from '@heroicons/react/20/solid';
+import { Button } from './button';
+import { authenticate } from '../lib/actions';
+
+export default function SignupForm() {
+  const [errorMessage, setErrorMessage] = useState("");
+  const router = useRouter();
+
+  const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const response = await authenticate(formData);
+    if (response.error) {
+      setErrorMessage(response.message);
+    } else {
+      router.push("/dashboard");
+    }
+  }
+
+  return (
+    <form className="space-y-3" onSubmit={handleFormSubmit}>
+      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+        <h1 className={`${montserrat.className} mb-3 text-2xl`}>
+          Please sign up to continue.
+        </h1>
+        <div className="w-full">
+          <div>
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Enter your email address"
+                defaultValue="user@nextmail.com"
+                required
+              />
+              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+          </div>
+          <div className="mt-4">
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="password"
+                type="password"
+                name="password"
+                placeholder="Enter password"
+                defaultValue="123456"
+                required
+                minLength={6}
+              />
+              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+          </div>
+        </div>
+        <Button className="mt-4 w-full" aria-disabled={false}>
+          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+        </Button>
+        <div className="flex h-8 items-end space-x-1">
+          {errorMessage && (
+            <p className="text-sm text-red-500">{errorMessage}</p>
+          )}
+        </div>
+      </div>
+    </form>
+  );
+}
