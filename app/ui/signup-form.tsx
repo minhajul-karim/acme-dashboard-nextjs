0d079fdcb8c4c481d8c9130afbf92a3758759a71
Minhajul Karim <project@zaagsys.com>
2025-11-02 13:02:44 +0600

    feat: add password visibility toggle to signup form

--- app/ui/signup-form.tsx
+++ app/ui/signup-form.tsx
@@ -1,30 +1,33 @@
 "use client";
 
-import { FormEvent, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { montserrat } from '@/app/ui/fonts';
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+import { montserrat } from "@/app/ui/fonts";
 import {
   AtSymbolIcon,
   KeyIcon,
-} from '@heroicons/react/24/outline';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
-import { authenticate } from '../lib/actions';
+  IdentificationIcon,
+  EyeIcon,
+  EyeSlashIcon,
+} from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { Button } from "./button";
+import { authenticate } from "../lib/actions";
 
 export default function SignupForm() {
   const [errorMessage, setErrorMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const router = useRouter();
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
 
   const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    const response = await authenticate(formData);
-    if (response.error) {
-      setErrorMessage(response.message);
-    } else {
-      router.push("/dashboard");
-    }
-  }
+    alert([...formData]);
+  };
 
   return (
     <form className="space-y-3" onSubmit={handleFormSubmit}>
@@ -33,6 +36,25 @@ export default function SignupForm() {
           Please sign up to continue.
         </h1>
         <div className="w-full">
+          <div>
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="name"
+            >
+              Name
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="name"
+                type="text"
+                name="name"
+                placeholder="Enter your name"
+                required
+              />
+              <IdentificationIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+          </div>
           <div>
             <label
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
@@ -47,7 +69,6 @@ export default function SignupForm() {
                 type="email"
                 name="email"
                 placeholder="Enter your email address"
-                defaultValue="user@nextmail.com"
                 required
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -64,19 +85,28 @@ export default function SignupForm() {
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="password"
-                type="password"
+                type={showPassword ? "text" : "password"}
                 name="password"
                 placeholder="Enter password"
-                defaultValue="123456"
                 required
-                minLength={6}
+                minLength={3}
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <div
+                className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
+                onClick={togglePasswordVisibility}
+              >
+                {showPassword ? (
+                  <EyeIcon className="h-[18px] w-[18px] text-gray-500" />
+                ) : (
+                  <EyeSlashIcon className="h-[18px] w-[18px] text-gray-500" />
+                )}
+              </div>
             </div>
           </div>
         </div>
         <Button className="mt-4 w-full" aria-disabled={false}>
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+          Sign up <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
         <div className="flex h-8 items-end space-x-1">
           {errorMessage && (
